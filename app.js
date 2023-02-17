b1e85d68a0607e6fffb01afeae36ba42f3f2266b
Aashrya Shrivastava <aashryashrivastava@gmail.com>
2023-02-17 21:59:31 +0530

    added css and created separate todolist for work stuff

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 var items = ["buy food", "cook food", "eat food"];
+var workItems = [];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.get("/", function (req, res) {
 
     var today = new Date();
@@ -13,7 +15,7 @@ app.get("/", function (req, res) {
         month: "long"
     };
     var day = today.toLocaleDateString("en-US", options);
-    res.render("list", { kindOfDay: day, newListItem: items });
+    res.render("list", { listTitle: day, newListItem: items });
 
 });
 
@@ -22,6 +24,16 @@ app.post("/", function (req, res) {
     console.log(item);
     items.push(item);
     res.redirect("/");
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItem: workItems });
+});
+
+app.post("/work", function (req, res) {
+    var item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 });
 
 app.listen(3000, function () {
